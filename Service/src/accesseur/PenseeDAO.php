008390498223ae6d8a1b03269a93a6c2e3bebc8b
Abdelhafid ENNES <45818418+Zab-an@users.noreply.github.com>
2019-02-21 19:29:19 -0500

    Génération de pensée aléatoire #2

--- Service/src/accesseur/PenseeDAO.php
+++ Service/src/accesseur/PenseeDAO.php
@@ -1,53 +1,45 @@
 <?php
-	include_once "basededonnees.php";
-	class PenseeDAO
-	{
-		function listerPensees()
-		{			
-			$SQL_LISTER_PENSEES = "SELECT * FROM pensee";
-			global $basededonnees;
-			$requeteListerPensees = $basededonnees->prepare($SQL_LISTER_PENSEES);
-			$requeteListerPensees->execute();
-			return $requeteListerPensees->fetchAll(PDO::FETCH_OBJ);
-		}
-        
-        function trouverPensee($numero)
-        {
-            $SQL_TROUVER_PENSEES = "SELECT * FROM pensee where idPensee =". $numero;
-            global $basededonnees;
-            $requeteTrouverPensee = $basededonnees->prepare($SQL_TROUVER_PENSEES);
-            $requeteTrouverPensee->execute();
-            return $requeteTrouverPensee->fetch(PDO::FETCH_OBJ);
-        }
+include_once "basededonnees.php";
+class PenseeDAO
+{
+    function listerPensees()
+    {
+        $SQL_LISTER_PENSEES = "SELECT * FROM pensee";
+        global $basededonnees;
+        $requeteListerPensees = $basededonnees->prepare($SQL_LISTER_PENSEES);
+        $requeteListerPensees->execute();
+        return $requeteListerPensees->fetchAll(PDO::FETCH_OBJ);
+    }
+    /*
+            function trouverPensees(numero)
+            {
+                $SQL_TROUVER_PENSEE = "SELECT * FROM pensee WHERE id = $numero";
+                global $basededonnees;
+                $requeteListerPensees = $basededonnees->prepare($SQL_TROUVER_PENSEE);
+                $requetePensees->execute();
+                return $requetePensee->fetch(PDO::FETCH_OBJ);
+            }
+        */
+    function ajouterPensee($pensee)
+    {
+        echo "ajouterPensee()";
+        print_r($pensee);
 
-        function getNombrePensees()
-        {
-            $SQL_NOMBRE_PENSEES = "SELECT count(*) nbr FROM pensee";
-            global $basededonnees;
-            $requeteNombrePensee = $basededonnees->prepare($SQL_NOMBRE_PENSEES);
-            $requeteNombrePensee->execute();
-            return $requeteNombrePensee->fetch(PDO::FETCH_OBJ);
+        $SQL_AJOUTER_PENSEE = "INSERT into pensee(auteur, message, annee) VALUES('$pensee->auteur','$pensee->message','$pensee->annee')";
 
-        }
-		
-		function ajouterPensee($pensee)
-		{
-			echo "ajouterPensee()";
-			print_r($pensee);
-			
-			$SQL_AJOUTER_PENSEE = "INSERT into pensee(auteur, message, annee) VALUES('$pensee->auteur','$pensee->message','$pensee->annee')";
-			
-			echo $SQL_AJOUTER_PENSEE;
-			global $basededonnees;
-			print_r($basededonnees);
-			
-			$requeteAjouterPensee = $basededonnees->prepare($SQL_AJOUTER_PENSEE);
-			$reussite = $requeteAjouterPensee->execute();
-			
-			echo "Code erreur : " . $basededonnees->errorCode();
-			echo "Erreurs : ";
-			print_r($basededonnees->errorInfo());
-			return $reussite;
-		}
-	}
+        echo $SQL_AJOUTER_PENSEE;
+        global $basededonnees;
+        print_r($basededonnees);
+
+        $requeteAjouterPensee = $basededonnees->prepare($SQL_AJOUTER_PENSEE);
+        $reussite = $requeteAjouterPensee->execute();
+
+        echo "Code erreur : " . $basededonnees->errorCode();
+        echo "Erreurs : ";
+        print_r($basededonnees->errorInfo());
+        return $reussite;
+    }
+
+
+}
 ?>
--- Service/src/accesseur/basededonnees.php
+++ Service/src/accesseur/basededonnees.php
@@ -5,11 +5,6 @@
 	$usager = "root";
 	$motdepasse = "bitnami";
 	$dsn = "mysql:dbname=".$base.";host=" . $hote;
-	try{
-		$basededonnees = new PDO($dsn, $usager, $motdepasse);
-	}
-	catch(PDOException $exception){
-		echo 'Failed';
-	}
+	$basededonnees = new PDO($dsn, $usager, $motdepasse);
 
 ?>
